cda0cf7286ef85ec55e96b2339aea67ed3f23337
Rafael Bergo <rafaelbergo503@gmail.com>
2023-03-12 21:51:29 -0300

    ponteiro modificado

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 #include <string.h>
 
-Pessoa::Pessoa(int diaNA, int mesNA, int anoNA, const char nome[]){
+Pessoa::Pessoa(int diaNA, int mesNA, int anoNA, const char* nome){
     diaP = diaNA;
     mesP = mesNA;
     anoP = anoNA;
     idadeP = -1;
     strcpy(nomeP, nome);
-    }
+}
 
 void Pessoa::calc_idade(int diaAT, int mesAT, int anoAT){
     idadeP = anoAT - anoP;
--- Pessoa.h
+++ Pessoa.h
@@ -7,7 +7,7 @@ class Pessoa{
         char nomeP[50];
 
     public:
-        Pessoa(int diaNA, int mesNA, int anoNA, const char nome[]);
+        Pessoa(int diaNA, int mesNA, int anoNA, const char* nome);
         void calc_idade(int diaAT, int mesAT, int anoAT);
         int getIdade();
 };
